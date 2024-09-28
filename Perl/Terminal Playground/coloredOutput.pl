cdc824fe34162caac6e4b311c2935c222314029f
Houston Brown <80162431+teagist@users.noreply.github.com>
2024-09-27 22:34:08 -0500

    Create coloredOutput.pl
    Script to test text color changes in the terminal.

---
+++ Perl/Terminal Playground/coloredOutput.pl
@@ -0,0 +1,9 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use Term::ANSIColor;
+
+print colored("This text should be red.\n", 'red');
+print colored("This text should be green.\n", 'green');
+print colored("This text should be bold and blue.\n", 'bold blue');
