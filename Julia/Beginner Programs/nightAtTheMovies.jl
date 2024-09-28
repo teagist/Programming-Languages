f65073dbb487f9c18cb1f187e36c6009efcc517d
Houston Brown <80162431+teagist@users.noreply.github.com>
2024-09-27 22:40:59 -0500

    Create nightAtTheMovies.jl

---
+++ Julia/Beginner Programs/nightAtTheMovies.jl
@@ -0,0 +1,17 @@
+println("Night at the Movies")
+println()
+
+adultTicket = 7.5
+childTicket = 4
+
+println("Please enter the name of the movie.")
+print(">")
+movieName = readline()
+
+println("Please enter the number of adult tickets sold.")
+print(">")
+adultTotal = readline()
+
+println("Please enter the number of children tickets sold.")
+print(">")
+childTotal = readline()
