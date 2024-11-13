b955c1dd60ef79beef8b958fe680a5def4af215e
Houston Brown <80162431+teagist@users.noreply.github.com>
2024-11-13 17:57:36 -0600

    Create main.cpp

---
+++ C++/SCO Simulation/main.cpp
@@ -0,0 +1,38 @@
+//****************************************************************************
+//	Driver File for Grocery Store Simulator
+//	Programmed by: Houston Brown
+//	Last Compiled Date: 11/13/2024
+//
+//	Copyright (c) 2024 Houston Brown
+//
+//	Permission is hereby granted to any person that obtains a copy of this
+//	software and associated documentation files (the "Software"), to deal
+//	within the Software for free and without restriction.  Including the
+//	rights to use, copy, modify, merge, publish, redistribute, sublicense,
+//	and/or sell copies of the Software.  Furthermore, to the people who the
+//	Software is furnished to do so, subject to the following:
+//
+//	The above copyright notice and this permission notice shall be included
+//	in all copies or substantial portions of the Software.
+//
+//	The Software is provided "as is", without any warranty of any kind,
+//	express or implied, including but not limited to the warranties of 
+//	merchantability, fitness for a particular purpose and noninfringement.
+//	In not event shall the liability, whether in action of contract, tort or
+//	otherwise, arising from, out of or in connection with the Software or the
+//	use or other dealings in the Software.
+//
+//	DISCLAIMER: This program uses system calls that will only work on 
+//				Windows operating system.
+//****************************************************************************
+
+#include "GroceryStore.h"
+
+
+
+int main()
+{
+	GroceryStore myStore;
+	myStore.runSimulation();
+	return 0;
+}
