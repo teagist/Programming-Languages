bb888174e494f503694e0c770fa448842555a815
Houston Brown <80162431+teagist@users.noreply.github.com>
2024-11-14 16:14:38 -0600

    Create WinUtils.h

---
+++ C++/Process Scheduler/WinUtils.h
@@ -0,0 +1,13 @@
+#ifndef _WINUTILS_H
+#define _WINUTILS_H
+
+#include <iostream>
+#include <sstream>
+#include <ctime>
+#include <windows.h>
+
+void delayBoot(int bootSeconds);
+void setTitleBar(std::string testcase);
+void clearScr();
+
+#endif
